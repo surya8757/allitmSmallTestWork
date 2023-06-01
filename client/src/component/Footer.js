89cec6ceb46fdec3f8a01b02860330474d890e22
surya <surya.pratap11122001@gmail.com>
2023-06-01 17:16:24 +0530

    this is itm project

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -4,7 +4,7 @@ const Footer=()=> {
   
   return (
     <div className='footer'>
-        Footer
+        @Footer
     </div>
   )
 }
--- client/src/component/Hooks.js
+++ client/src/component/Hooks.js
@@ -4,8 +4,9 @@ import "./Hooks.css";
 const Hooks=()=> {
   return (
     <>
-    {/* <img src="https://images.unsplash.com/photo-1595147389795-37094173bfd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGltYWdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="picture" height="100%" width="100%" */}
-    {/* /> */}
+    <div className='image-container'>
+    <img src="https://images.unsplash.com/photo-1595147389795-37094173bfd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGltYWdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="picture"/>
+    </div>
     </>
   );
 }
